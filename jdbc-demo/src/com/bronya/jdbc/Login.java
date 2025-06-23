0597437c65fc642cbeaf559aa111d611e53a5ce2
Bro-nya <1572205083@qq.com>
2025-06-24 02:47:42 +0800

    3

--- jdbc-demo/src/com/bronya/jdbc/Login.java
+++ jdbc-demo/src/com/bronya/jdbc/Login.java
@@ -2,14 +2,15 @@ package com.bronya.jdbc;
 
 import org.junit.Test;
 
+import java.sql.*;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
 
 public class Login {
 
+    /// 会被sql注入的登录
     @Test
     public void testLogin() throws Exception {
         String url = "jdbc:mysql://localhost:3306/test?useSSL=false";
@@ -37,5 +38,36 @@ public class Login {
         conn.close();
     }
 
+    /// 防止sql注入，preparedStatement
+    @Test
+    public void testLogin_Inject() throws Exception {
+        //useServerPrepStmts=true 开启预编译
+        String url = "jdbc:mysql://localhost:3306/test?useSSL=false&useServerPrepStmts=true";
+        String username = "root";
+        String password = "1234";
+        Connection conn = DriverManager.getConnection(url, username, password);
+
+        String name="bronya";
+        String pwd="123456";
+
+        String sql = "select * from tb_user where name=? and pwd=?";
+
+        //创建链接对象，并传入sql值
+        PreparedStatement stmt=conn.prepareStatement(sql);
+        //设置？的值
+        stmt.setString(1,name);
+        stmt.setString(2,pwd);
+
+        ResultSet rs=stmt.executeQuery();
+
+        if (rs.next()) {
+            System.out.println("成功");
+        }else {
+            System.out.println("失败");
+        }
+        rs.close();
+        stmt.close();
+        conn.close();
+    }
 
 }
